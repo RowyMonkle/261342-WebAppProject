4eb3757768378a328e7e5c024e244fe7c86b4e33
FameNan <nannapat_chaip@cmu.ac.th>
2026-03-16 00:11:02 +0700

    trust proxies for railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        //for not show form not secure
+        $middleware->trustProxies(at:'*');
         $middleware->alias([
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ]);
